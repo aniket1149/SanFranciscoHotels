ae011e2cdfd16add676c29341859ba7650f6fdd4
Aniket Thakker @USF <athakker@dons.usfca.edu>
2024-12-07 18:38:41 -0800

    History Repository Creation

---
+++ src/main/java/server/repositories/HistoryRepository.java
@@ -0,0 +1,11 @@
+package server.repositories;
+
+import hotelapp.models.History;
+
+import java.util.List;
+
+public interface HistoryRepository {
+    boolean saveHistory(History history);
+    List<History> findUserClickHistory(String userName);
+    boolean updateHistory(History history, String username);
+}
