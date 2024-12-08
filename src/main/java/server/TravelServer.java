2a421dc1feee4d148ccffb4b00c16de92619d537
Aniket Thakker @USF <athakker@dons.usfca.edu>
2024-12-07 18:42:22 -0800

    Removing dependecies and migrating to db repository structure.

--- src/main/java/server/TravelServer.java
+++ src/main/java/server/TravelServer.java
@@ -9,6 +9,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import server.repositories.*;
 import server.servlets.*;
 
 import java.util.List;
@@ -21,10 +22,18 @@ public class TravelServer {
     private Server jettyServer;
     private ServletContextHandler handler;
     private static final Logger logger = LogManager.getLogger(TravelServer.class);
-    public TravelServer() {
+    private HotelRepository hotelRepository;
+    private ReviewRepository reviewRepository;
+    private UserRepository userRepository;
+    private HistoryRepository historyRepository;
+    public TravelServer(HotelRepository hotelRepository, ReviewRepository reviewRepository, UserRepository userRepository, HistoryRepository historyRepository) {
         jettyServer = new Server(PORT);
         handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         jettyServer.setHandler(handler);
+        this.hotelRepository = hotelRepository;
+        this.reviewRepository = reviewRepository;
+        this.userRepository = userRepository;
+        this.historyRepository = historyRepository;
     }
 
     /**
@@ -32,28 +41,33 @@ public class TravelServer {
      * **/
 
     public void addServlets(List<Object> objs){
+        /**
+         * TO-DO
+         * **/
         handler.setContextPath("/");
-        handler.addServlet(UserServlet.class, "/user/*");
-        HotelCollection hotelCollection = null;
-        ThreadSafeInvertedIndex reviewCollection=null;
-        for(Object obj : objs){
-            if(obj instanceof HotelCollection){
-                hotelCollection = (HotelCollection) obj;
-                handler.addServlet(new ServletHolder(new SearchServlet((HotelCollection) obj)), "/search/*");
-            }else if(obj instanceof ThreadSafeInvertedIndex){
-                reviewCollection = (ThreadSafeInvertedIndex) obj;
-                ReviewServlet reviewServlet = new ReviewServlet(reviewCollection);
-                ServletHolder reviewServletHolder = new ServletHolder(reviewServlet);
-                handler.addServlet(reviewServletHolder, "/review/*");
-            }else{
-                logger.error("Error intializing the servlets");
-                return;
-            }
+        handler.addServlet(new ServletHolder(new UserServlet(userRepository)), "/user/*");
+        handler.addServlet(new ServletHolder(new HistoryServlet(historyRepository, hotelRepository)), "/history/*");
+        handler.addServlet(new ServletHolder(new SearchServlet(hotelRepository)), "/search/*");
+        handler.addServlet(new ServletHolder(new ReviewServlet(reviewRepository)), "/review/*");
+        handler.addServlet(new ServletHolder(new HotelServlet(hotelRepository, reviewRepository)), "/hotel/*");
 
-        }
-        if(hotelCollection != null && reviewCollection != null){
-            handler.addServlet(new ServletHolder(new HotelServlet(hotelCollection, reviewCollection)), "/hotel/*");
-        }
+//        HotelCollection hotelCollection = null;
+//        ThreadSafeInvertedIndex reviewCollection=null;
+//        for(Object obj : objs){
+//            if(obj instanceof HotelCollection){
+//                hotelCollection = (HotelCollection) obj;
+//            }else if(obj instanceof ThreadSafeInvertedIndex){
+//                reviewCollection = (ThreadSafeInvertedIndex) obj;
+//                ReviewServlet reviewServlet = new ReviewServlet(reviewRepository);
+//                ServletHolder reviewServletHolder = new ServletHolder(reviewServlet);
+//            }else{
+//                logger.error("Error intializing the servlets");
+//                return;
+//            }
+//
+//        }
+//        if(hotelCollection != null && reviewCollection != null){
+//        }
     }
 
     public void start() throws Exception {
--- src/main/java/server/TravelServerDriver.java
+++ src/main/java/server/TravelServerDriver.java
@@ -5,23 +5,27 @@ import hotelapp.controller.HotelCollection;
 import hotelapp.controller.ThreadSafeInvertedIndex;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import server.repositories.UserRepositoryImpl;
+import server.repositories.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TravelServerDriver {
-    public static final int PORT = 8080;
     private static final Logger logger = LogManager.getLogger(TravelServerDriver.class);
-
+    private static final HotelRepository hotelRepository = new HotelRepositoryImpl();
+    private static final ReviewRepository reviewRepository = new ReviewRepositoryImpl();
+    private static final UserRepository userRepository = new UserRepositoryImpl();
+    private static final HistoryRepository historyRepository = new HistoryRepositoryImpl();
 
     public static void main(String[] args)  {
         // FILL IN CODE, and add more classes as needed
-        HotelReviewService hotelReviewService = new HotelReviewService();
+
+
+        HotelReviewService hotelReviewService = new HotelReviewService(hotelRepository, reviewRepository, userRepository);
         hotelReviewService.loadData(args);
         HotelCollection hotelCollection = hotelReviewService.getHotelCollection();
         ThreadSafeInvertedIndex reviewCollection = hotelReviewService.getReviewCollection();
-        TravelServer server = new TravelServer();
+        TravelServer server = new TravelServer(hotelRepository, reviewRepository, userRepository, historyRepository);
         List<Object> dataObjects = new ArrayList<>();
         dataObjects.add(hotelCollection);
         if (reviewCollection != null) {
